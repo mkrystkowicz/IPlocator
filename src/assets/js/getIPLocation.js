d8187eef558664f572f3cfae06666b8473bdfd71
Mateusz <krissie515@gmail.com>
2021-03-25 16:10:58 +0100

    get ip-information passed by user

---
+++ src/assets/js/getIPLocation.js
@@ -0,0 +1,15 @@
+import 'regenerator-runtime/runtime';
+require('dotenv').config();
+
+export default async function getIPLocation(IPAddress) {
+  const URL = `https://geo.ipify.org/api/v1?apiKey=${process.env.GEO_API_KEY}&ipAddress=${IPAddress}`;
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    throw new Error('err', err);
+  }
+}
