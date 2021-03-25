b2e1bebd57ff158c312346804e2275556b701593
Mateusz <krissie515@gmail.com>
2021-03-25 18:34:53 +0100

    add basic navigation - input,map handling

---
+++ src/assets/js/index.js
@@ -0,0 +1,33 @@
+import DOMElements from './DOMElements';
+import Input from './Input';
+import getIPLocation from './getIPLocation';
+import IPInfoValues from './IPInfoValues';
+import Map from './Map';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const map = new Map(57, 13);
+  const IPInfo = new IPInfoValues();
+  map.setMap();
+  map.setTileLayer();
+
+  DOMElements.btn.addEventListener('click', () => render(map, IPInfo));
+});
+
+function render(map, IPInfo) {
+  const input = new Input();
+  const ipAddress = input.getValue();
+
+  getIPLocation(ipAddress).then(async data => {
+    const {
+      location: { lat, lng, city, region, timezone },
+      ip,
+      isp,
+    } = await data;
+
+    IPInfo.setValues(ip, city, region, timezone, isp);
+    IPInfo.getValues();
+
+    map.setCoordinates(lat, lng);
+    map.setCircle();
+  });
+}
