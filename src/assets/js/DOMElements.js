fdcf96b15b25ca263fa3ceebff35e3480fa7bc3f
Mateusz <krissie515@gmail.com>
2021-03-25 16:09:25 +0100

    get DOMElements

---
+++ src/assets/js/DOMElements.js
@@ -0,0 +1,10 @@
+const DOMElements = {
+  input: document.getElementById('ip-input'),
+  btn: document.getElementById('ip-btn'),
+  ipAddressLabel: document.getElementById('ip-address-label'),
+  ipLocationLabel: document.getElementById('ip-location-label'),
+  ipTimezoneLabel: document.getElementById('ip-timezone-label'),
+  ipISPLabel: document.getElementById('ip-isp-label'),
+};
+
+export default DOMElements;
