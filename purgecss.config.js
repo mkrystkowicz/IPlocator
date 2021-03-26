d28ffc60ea9d1d8754ef87f2f16ce16946225afe
Mateusz <krissie515@gmail.com>
2021-03-26 12:56:30 +0100

    fix build config to purge html (only needed classes)

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    content: ["./src/**/*.html", "./src/**/*.svg", "./src/**/*.js"],
-    extractors: [
-        {
-            extensions: ["html", "svg", "js"],
-            extractor: class TailwindExtractor {
-                static extract(content) {
-                    return content.match(/[A-Za-z0-9-_:/]+/g) || [];
-                }
-            },
-        },
-    ],
+  content: ['./src/**/*.html', './src/**/*.svg', './src/**/*.js'],
+  extractors: [
+    {
+      extensions: ['html', 'svg', 'js'],
+      extractor: class TailwindExtractor {
+        static extract(content) {
+          return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+        }
+      },
+    },
+  ],
 };
