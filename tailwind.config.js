e8608bc2bc89d5a09d5a2da7d8321f2f9388efba
Mateusz <krissie515@gmail.com>
2021-03-26 13:21:55 +0100

    add purge for js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  purge: ['./src/**/*.html'],
+  purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
       sans: ['Nunito', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    boxShadow: {
+      inner: 'inset 0 0 4px 1px #B91C1C',
     },
   },
   variants: {
